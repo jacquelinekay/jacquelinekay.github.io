82374b1f629bd7a9169b45023120829d341116e5
Jackie Kay <jackie@vulcan>
2014-06-23 08:46:34 -0400

    Fixed header

--- header.js
+++ header.js
@@ -6,7 +6,7 @@ document.write('\
     <li><a href="index.html">about</a></li>\
     <li><a href="cv.html">cv</a></li>\
     <li><a href="projects.html">projects</a></li>\
-    <li><a href="blog.html">blog</a></li>\
+    <li><a href="http://jackieokay.blogspot.com">blog</a></li>\
     <li><a href="links.html">links</a></li>\
     </ul>\
   </header>\
