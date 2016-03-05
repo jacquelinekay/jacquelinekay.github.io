35c10081cffc7a05a8bfa4510178f3947d50df1a
Jackie Kay <jacquelinekay1@gmail.com>
2016-03-05 11:20:31 -0800

    Replace old files with template

--- resize.js
+++
@@ -1,9 +0,0 @@
-function autoResize(id){
-    var newheight;
-
-    if(document.getElementById){
-        newheight=document.getElementById(id).contentWindow.document .body.scrollHeight;
-    }
-
-    document.getElementById(id).height= (newheight) + "px";
-}
